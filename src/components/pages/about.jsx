4b599293d35a88322f740ea692c5ccc65deee242
Mcas4150 <mikecassidy0000@gmail.com>
2021-05-17 21:56:37 -0400

    first media query

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -6,7 +6,9 @@ export default class About extends Component {
     return (
       <div>
         <PageHeader/>
-          About Page
+          <p>XP Gaming Bar is a video game themed bar founded by gamers for gamers. Here you can enjoy high quality cocktails and comfort food while playing the latest video games and board games with your friends</p><p>Whether it's taking down the covenant on the Xbox Series X, swinging through New York as Spiderman on the PS5, racing your friends in Mario Cart on the Nintendo Switch, or having a quick duel against your friends to show them who's the true king of games is, there is something for every gamer at XP Gaming Bar!
+            #LevelUp!
+          </p>
       </div>
     )
   }
--- src/components/pages/menu.jsx
+++ src/components/pages/menu.jsx
@@ -1,243 +1,266 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import PageHeader from "./pageHeader";
 
-export default class Menu extends Component {
-  render() {
-    return (
-      <div>
-        <PageHeader />
-        <MenuContainer>
-          <MenuBorder>
-            <Tables>
-              <Food>
-                <THeader>Food</THeader>
-                <TList>
-                  <TItem>
-                    <Info>
-                      <Name>Mozzarella Sticks</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Onion Rings</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>French Fries</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Chicken Fingers</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Chicken Quesadilla</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Cheese Quesadilla</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Cheeseburger</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>BBQ Cheeseburger</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Breakfast Burger</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Chicken Burger</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Midnight Burger</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    <Info>
-                      <Name>Double Cheeseburger</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                </TList>
-              </Food>
-              <Drinks>
-                <THeader>Drinks</THeader>
-                <TList>
-                  <TItem>
-                    <Info>
-                      <Name>Mana Beer</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Potion Beer</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Take Your Heart</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Sheogorath</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Electric Rat</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>117</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Covenant</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Princess</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Frag Grenade</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Protector</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Hunter</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Rainbow Cocktail</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Radiation</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Pandora</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Cosmopolitan</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                  <TItem>
-                    {" "}
-                    <Info>
-                      <Name>Mohito</Name>
-                      <Description>Lorum Ipsum</Description>
-                    </Info>
-                    <Price>500</Price>
-                  </TItem>
-                </TList>
-              </Drinks>
-            </Tables>
-          </MenuBorder>
-        </MenuContainer>
-      </div>
-    );
-  }
-}
+export const Menu = () => {
+  const [dimensions, setDimensions] = useState({
+    height: document.documentElement.clientHeight,
+    width: document.documentElement.clientWidth,
+  });
+
+  let TileStyle =
+    dimensions.width <= 800 || dimensions.height <= 600
+      ? TileStyleMin
+      : TileStyleMax;
+
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return (_) => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  return (
+    <div>
+      <PageHeader />
+      <MenuContainer>
+        <MenuBorder>
+          <Tables style={TileStyle}>
+            <Food>
+              <THeader>Food</THeader>
+              <TList>
+                <TItem>
+                  <Info>
+                    <Name>Mozzarella Sticks</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Onion Rings</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>French Fries</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Chicken Fingers</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Chicken Quesadilla</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Cheese Quesadilla</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Cheeseburger</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>BBQ Cheeseburger</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Breakfast Burger</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Chicken Burger</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Midnight Burger</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  <Info>
+                    <Name>Double Cheeseburger</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+              </TList>
+            </Food>
+            <Drinks>
+              <THeader>Drinks</THeader>
+              <TList>
+                <TItem>
+                  <Info>
+                    <Name>Mana Beer</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Potion Beer</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Take Your Heart</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Sheogorath</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Electric Rat</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>117</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Covenant</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Princess</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Frag Grenade</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Protector</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Hunter</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Rainbow Cocktail</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Radiation</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Pandora</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Cosmopolitan</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+                <TItem>
+                  {" "}
+                  <Info>
+                    <Name>Mohito</Name>
+                    <Description>Lorum Ipsum</Description>
+                  </Info>
+                  <Price>500</Price>
+                </TItem>
+              </TList>
+            </Drinks>
+          </Tables>
+        </MenuBorder>
+      </MenuContainer>
+    </div>
+  );
+};
 
 const MenuContainer = styled.div`
   width: 75vw;
@@ -250,9 +273,6 @@ const MenuBorder = styled.div`
 
 const Tables = styled.div`
   display: grid;
-
-  grid-template-columns: 1fr 1fr;
-  grid-template-areas: "food drinks";
 `;
 const Table = styled.div`
   padding: 15px;
@@ -285,3 +305,13 @@ const Price = styled.div``;
 const Description = styled.div`
   font-size: 0.85rem;
 `;
+
+const TileStyleMax = {
+  gridTemplateAreas: `'food drinks'`,
+  gridTemplateColumns: "1fr 1fr",
+};
+
+const TileStyleMin = {
+  gridTemplateAreas: `'food' 'drinks'`,
+  gridTemplateColumns: "1fr",
+};
